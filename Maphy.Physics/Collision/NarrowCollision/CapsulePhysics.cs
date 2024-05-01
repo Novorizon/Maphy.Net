ccf206823d0a18b0175a3b9244d448fdbb471166
Guan Jinbiao <Novorizon@hotmail.com>
2024-05-01 11:21:51 +0800

    Update CapsulePhysics.cs
    * capsule

--- Maphy.Physics/Collision/NarrowCollision/CapsulePhysics.cs
+++ Maphy.Physics/Collision/NarrowCollision/CapsulePhysics.cs
@@ -1,5 +1,6 @@
 
 using Maphy.Mathematics;
+using static UnityEditor.IMGUI.Controls.PrimitiveBoundsHandle;
 
 namespace Maphy.Physics
 {
@@ -54,6 +55,58 @@ namespace Maphy.Physics
                     return true;
             }
 
+            return false;
+        }
+
+        //https://wickedengine.net/2020/04/capsule-collision-detection/
+        internal static bool Overlaps(Capsule a, Capsule b)
+        {// capsule A:
+            fix3 a_Normal = math.normalize(a.Center2 - a.Center1);
+            fix3 a_LineEndOffset = a_Normal * a.Radius;
+            fix3 a_A = a.Center1 + a_LineEndOffset;
+            fix3 a_B = a.Center2 - a_LineEndOffset;
+
+            // capsule B:
+            fix3 b_Normal = math.normalize(b.Center2 - b.Center1);
+            fix3 b_LineEndOffset = b_Normal * b.Radius;
+            fix3 b_A = b.Center1 + b_LineEndOffset;
+            fix3 b_B = b.Center2 - b_LineEndOffset;
+
+            // vectors between line endpoints:
+            fix3 v0 = b_A - a_A;
+            fix3 v1 = b_B - a_A;
+            fix3 v2 = b_A - a_B;
+            fix3 v3 = b_B - a_B;
+
+            // squared distances:
+            fix B1A1 = math.dot(v0, v0);
+            fix B2A1 = math.dot(v1, v1);
+            fix B1A2 = math.dot(v2, v2);
+            fix B2A2 = math.dot(v3, v3);
+
+            // select best potential endpoint on capsule A:
+            fix3 bestA;
+            if (B1A2 < B1A1 || B1A2 <B2A1  || B2A2 < B1A1 || B2A2 < B2A1)
+            {
+                bestA = a_B;
+            }
+            else
+            {
+                bestA = a_A;
+            }
+
+            // select point on capsule B line segment nearest to best potential endpoint on A capsule:
+            fix3 bestB = GetClosestPointOnSegment(b_A, b_B, bestA);
+
+            // now do the same for capsule A segment:
+            bestA = GetClosestPointOnSegment(a_A, a_B, bestB);
+            // We selected the two best possible candidates on both capsule axes.What remains is to place spheres on those points and perform the sphere intersection routine:
+
+            fix3 penetration_normal = bestA - bestB;
+            float len = math.length(penetration_normal);
+            penetration_normal /= len;  // normalize
+            float penetration_depth = a.Radius + b.Radius - len;
+            bool intersects = penetration_depth > 0;
             return false;
         }
     }
